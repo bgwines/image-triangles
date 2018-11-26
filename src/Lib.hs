15c2ce11e0accd42a644757fd838d0b2ce590060
Brett Wines <brettwines@gmail.com>
2018-11-25 21:53:04 -0500

    Display images
        $ stack ghci
        > main  -- displays image

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
-    ) where
+( displayGradGrayImage
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import Graphics.Image
+-- import Graphics.Image.Interface.Elevator
+
+
+makeGradGrayImage :: Image RPU Y Double
+makeGradGrayImage = makeImageR RPU (200, 200) (\(i, j) -> PixelY $ fromIntegral (i*j)) / (200*200)
+
+displayGradGrayImage :: IO ()
+displayGradGrayImage = displayImage makeGradGrayImage
